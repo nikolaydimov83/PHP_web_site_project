536860cba1b6f0702ccc757699802007707f5d73
DimitarRuskov <mitko_ruskov@abv.bg>
2014-08-25 03:03:48 +0300

    Fully functional forum

--- beta/connect.php
+++ beta/connect.php
@@ -1,11 +1,3 @@
-
-//need to set up connections between different tables: put the following script in the database SQL
-//ALTER TABLE topics ADD FOREIGN KEY(topic_cat) REFERENCES categories(cat_id) ON DELETE CASCADE ON UPDATE CASCADE;
-//ALTER TABLE topics ADD FOREIGN KEY(topic_by) REFERENCES users(user_id) ON DELETE RESTRICT ON UPDATE CASCADE;
-//ALTER TABLE posts ADD FOREIGN KEY(post_topic) REFERENCES topics(topic_id) ON DELETE CASCADE ON UPDATE CASCADE;
-//ALTER TABLE posts ADD FOREIGN KEY(post_by) REFERENCES users(user_id) ON DELETE RESTRICT ON UPDATE CASCADE;
-
-
 <?php
 session_start();
 //connect.php
--- beta/header.php
+++ beta/header.php
@@ -9,12 +9,12 @@
 	<link rel="stylesheet" href="style.css" type="text/css">
 </head>
 <body>
-<h1>My forum</h1>
+<h1>Forum borum fushkii</h1>
 	<div id="wrapper">
 	<div id="menu">
-		<a class="item" href="/forum/index.php">Home</a> -
-		<a class="item" href="/forum/create_topic.php">Create a topic</a> -
-		<a class="item" href="/forum/create_cat.php">Create a category</a>
+		<a class="item" href="index.php">Home</a> -
+		<a class="item" href="create_topic.php">Create a topic</a> -
+		<a class="item" href="create_cat.php">Create a category</a>
 
         <div id="userbar">
             <?php
