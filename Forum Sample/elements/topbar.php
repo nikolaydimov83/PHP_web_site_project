6f159464ef6e79260e903166de0088cad536a931
DimitarRuskov <mitko_ruskov@abv.bg>
2014-08-27 13:24:18 +0300

    Refactoring and redirecting of buttons

--- Forum Sample/elements/topbar.php
+++ Forum Sample/elements/topbar.php
@@ -1,5 +1,5 @@
 <?php 
-define("ROOT_URL", 'http://' . $_SERVER['HTTP_HOST'] . '/forom/');
+define("ROOT_URL", 'http://' . $_SERVER['HTTP_HOST'] . '/Forum Sample/');
 define("ROOT_PATH", dirname(__FILE__));
 $sesstat = session_status();
 if (empty($_SESSION) && $sesstat == PHP_SESSION_NONE) {
